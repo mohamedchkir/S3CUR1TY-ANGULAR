15424bffd2af20d27ef043c3a6e9fca6bd142d4b
mohamed chkir <mohamedchkir2@gmail.com>
2024-02-08 14:08:57 +0100

    create routes

--- S3CUR1TY_Angular/src/app/app.routes.ts
+++ S3CUR1TY_Angular/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import path from "node:path";
+import {RegisterComponent} from "./component/register/register.component";
+import {LoginComponent} from "./component/login/login.component";
+import {DashboardComponent} from "./component/dashboard/dashboard.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: "login", pathMatch: "full"},
+  {path: 'register',component:RegisterComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'dashboard', component:DashboardComponent}
+
+]
