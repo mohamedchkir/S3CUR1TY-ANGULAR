fb3bf5fb386c6e4d2c53741484638e98bfce7904
mohamed chkir <mohamedchkir2@gmail.com>
2024-02-14 09:20:30 +0100

    ajouter guard et ausi travailler la page login front

--- S3CUR1TY_Angular/src/app/app.routes.ts
+++ S3CUR1TY_Angular/src/app/app.routes.ts
@@ -1,13 +1,13 @@
 import { Routes } from '@angular/router';
-import path from "node:path";
 import {RegisterComponent} from "./component/register/register.component";
 import {LoginComponent} from "./component/login/login.component";
 import {DashboardComponent} from "./component/dashboard/dashboard.component";
+import {authGuard} from "./guard/auth.guard";
 
 export const routes: Routes = [
   {path: '', redirectTo: "login", pathMatch: "full"},
   {path: 'register',component:RegisterComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'dashboard', component:DashboardComponent}
+  {path: 'dashboard', component:DashboardComponent, canActivate: [authGuard]}
 
 ]
--- S3CUR1TY_Angular/src/app/guard/auth.guard.ts
+++ S3CUR1TY_Angular/src/app/guard/auth.guard.ts
@@ -1,5 +1,19 @@
 import { CanActivateFn } from '@angular/router';
+import {inject} from "@angular/core";
+import {AuthService} from "../service/auth.service";
+import {Router} from "express";
 
 export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+  const authService = inject(AuthService);
+
+  if (authService.isAuthenticated()) {
+    return true;
+  } else {
+    const url = state.url.toString() // Extract URL string
+    const returnUrl = encodeURIComponent(url); // Encode for safe query parameter
+
+    inject(Router).navigate(['/login'], { queryParams: { returnUrl } });
+    return false;
+
+  }
 };
